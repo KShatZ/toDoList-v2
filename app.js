8aa74e4e793cff84b25a40aec26d0b78cc7ce83a
Kirill <cierio.dev@gmail.com>
2022-01-21 11:38:43 -0500

    Remove body parser code and change to built in express middleware

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-//jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 const items = ["Buy Food", "Cook Food", "Eat Food"];
